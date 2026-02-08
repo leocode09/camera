3ccfb9539159dc3dc1bb8d2fce5ba92d41097993
leocode09 <leocode09@gmail.com>
2026-02-08 02:21:40 +0200

    Update 2 files

--- lib/platform/windows_camera.dart
+++ lib/platform/windows_camera.dart
@@ -1,4 +1,3 @@
-import 'package:camera_platform_interface/camera_platform_interface.dart';
 import 'package:camera_windows/camera_windows.dart';
 import 'package:flutter/foundation.dart';
 
@@ -6,5 +5,5 @@ void registerWindowsCamera() {
   if (defaultTargetPlatform != TargetPlatform.windows) {
     return;
   }
-  CameraPlatform.instance = CameraWindows();
+  CameraWindows.registerWith();
 }
